7df9212df50bfd54dc79a2ea38a72915c9d5355d
John Black <john.f.black@icloud.com>
2016-09-18 10:39:32 -0400

    migrated my native editor tool over to tab 2

--- angular.js
+++ angular.js
@@ -1,5 +1,29 @@
 var myApp = angular.module("myApp", []);
 
+myApp.directive("anTag", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'anTag.html'
+   } 
+});
+
+myApp.directive("anAd", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'anAd.html'
+   } 
+});
+
+
+myApp.controller('myCtrl', function($scope) {
+    $scope.placementid = "PLACEMENT_ID";
+    $scope.ctacolor = "rgb(108, 135, 216)";
+    $scope.fontfamily = "Arial, Helvetica, sans-serif";
+    $scope.subFontWeight = "800";
+    $scope.titFontWeight = "700";
+    $scope.ctaFontWeight = "700";
+    });
+
 myApp.directive("defaultNativeTag", function(){
    return {
        restrict: 'E',
--- jquery.js
+++ jquery.js
@@ -36,6 +36,94 @@ $(".adchoice_wht").click(function(){
 });
     
     
+
+
+            
+            
+/* ------ Button Code to Download Tag ------ */
+
+
+        $(document).on("click", ".tagButton", function(){
+
+            var newTag = $(".box_right").text();
+            console.log(newTag);
+
+            var hiddenElement = document.createElement('a');
+
+            hiddenElement.href = 'data:attachment/text,' + encodeURI(newTag);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'audience_network_tag.txt';
+            hiddenElement.click();
+        });
+
+
+/* ------ Button Code to Update CSS ------ */
+            
+            
+        $(".ctaButton").click(function(){
+            console.log("CTA Button Click");
+
+            var ctaColorNew = $("#ctaCol").val();
+            console.log(ctaColorNew);
+
+            $(".thirdPartyCallToActionClass").css({"color": ctaColorNew});
+        });
+
+        $(".fontFamButton").click(function(){
+           console.log("Font Family Button Click");
+
+            var fontFamNew = $("#fontFam").val();
+            console.log(fontFamNew);
+
+            $(".thirdPartyRoot").css({"font-family": fontFamNew});
+        });
+
+        $(".subWeightButton").click(function(){
+           console.log("Subtitle Weight Button Click"); 
+
+            var subWeNew = $("#subFontWeight").val();
+            console.log(subWeNew);
+
+            $(".fbAdSubtitle").css({"font-weight": subWeNew});
+        });
+
+        $(".titWeightButton").click(function(){
+            console.log("Title Weight Button Click"); 
+
+            var titWeNew = $("#titFontWeight").val();
+            console.log(titWeNew);
+
+            $(".fbAdTitle").css({"font-weight": titWeNew});
+        });
+
+        $(".ctaWeightButton").click(function(){
+            console.log("CTA Weight Button Click"); 
+
+            var ctaWeNew = $("#ctaFontWeight").val();
+            console.log(ctaWeNew);
+
+            $(".thirdPartyCallToActionClass").css({"font-weight": ctaWeNew});
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
 });
 
 
