681242469d458d4d733a97e066beecca111a3828
John Black <john.f.black@icloud.com>
2017-09-06 20:38:04 -0400

    changing site to postbid test

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="author" content="John F. Black">
+        <meta name="description" content="Test Site for John F. Black">
+        <meta name="keywords" content="John, John Black, John F Black, John F. Black, John Francis Black, Test Site, Test, Synacor, Facebook, NBC, NBCUniversal, LocalEdge, Buffalo, New York, New York City, NYC, Grand Island, University at Buffalo, UB, Technical Support, Ad Operations, AdOps, AdTech">
+        <title>BlackOpsJohn</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+        <script>
+          var googletag = googletag || {};
+          googletag.cmd = googletag.cmd || [];
+        </script>
+
+        <script>
+          googletag.cmd.push(function() {
+            googletag.defineSlot('/27721068/postbid_test', [300, 250], 'div-gpt-ad-1504744251841-0').addService(googletag.pubads());
+            googletag.enableServices();
+          });
+        </script>
+
+    </head>
+    <body>
+      <hr>
+      <div class="details">
+        <h4>- Prebid.js v26 -</h4>
+        <h4>- Audience Network Postbid Test -</h4>
+      </div>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <hr>
+      <div class="adSlot1" style='height:250px; width:300px;'>
+        <div id='div-gpt-ad-1504744251841-0' style='height:250px; width:300px;'>
+          <script>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1504744251841-0'); });
+          </script>
+        </div>
+      </div>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <hr>
+    </body>
+
+</html>
